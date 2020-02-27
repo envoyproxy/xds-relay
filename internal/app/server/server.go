9af72fc1bccb964f0890460924dc22459b2f51a9
Jess Yuen <jyuen@lyft.com>
2020-02-26 16:39:24 -0800

    Use context.WithCancel()
    Signed-off-by: Jess Yuen <jyuen@lyft.com>

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -32,8 +32,12 @@ func Run() {
 	logger := log.New().Sugar()
 
 	// Cursory implementation of go-control-plane's server.
+	// TODO cancel should be invoked by shutdown handlers.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	snapshotCache := cache.NewSnapshotCache(false, cache.IDHash{}, nil)
-	gcpServer := gcp.NewServer(context.Background(), snapshotCache, nil)
+	gcpServer := gcp.NewServer(ctx, snapshotCache, nil)
 	server := grpc.NewServer()
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
