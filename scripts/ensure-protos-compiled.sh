7bedeb873ba58100f55fd4c667b3a1c5e6f19ebc
eapolinario <eapolinario@lyft.com>
2020-03-05 17:38:35 -0800

    Rename ci script to ensure generated protos match proto definition.
    Signed-off-by: eapolinario <eapolinario@lyft.com>

--- scripts/ensure-protos-compiled.sh
+++ scripts/ensure-protos-compiled.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This script ensures that there is no discrepancy between proto definition found in api/protos
+# and the generated proto files under pkg/api.
+
 cd "$(git rev-parse --show-toplevel)"
 modified=$(git status --porcelain pkg)
 if [[ -n "${modified}" ]]; then
