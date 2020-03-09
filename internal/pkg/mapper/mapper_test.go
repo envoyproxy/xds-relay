b13fe71cb833558baff0fc3e386103c2655decc4
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-03-09 16:12:24 -0700

    first UT
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- internal/pkg/mapper/mapper_test.go
+++ internal/pkg/mapper/mapper_test.go
@@ -1,15 +1,42 @@
 package mapper
 
 import (
+	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Zap options setup", func() {
-	BeforeEach(func() {
-	})
+type KeyerConfiguration = aggregationv1.KeyerConfiguration
+type Fragment = aggregationv1.KeyerConfiguration_Fragment
+type FragmentRule = aggregationv1.KeyerConfiguration_Fragment_Rule
+type MatchPredicate = aggregationv1.MatchPredicate
+type ResultPredicate = aggregationv1.ResultPredicate
 
-	It("should set a custom writer", func() {
-		Expect(0).To(Equal(0))
+var _ = Describe("Test1", func() {
+	It("Test it 2", func() {
+		protoConfig := KeyerConfiguration{
+			Fragments: []*Fragment{
+				&Fragment{
+					Rules: []*FragmentRule{
+						&FragmentRule{
+							Match: &MatchPredicate{
+								Type: &aggregationv1.MatchPredicate_AnyMatch{
+									AnyMatch: true,
+								},
+							},
+							Result: &ResultPredicate{
+								Type: &aggregationv1.ResultPredicate_StringFragment{
+									StringFragment: "abc",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		mapper := NewMapper(protoConfig)
+		key, _ := mapper.GetKeys(core.Node{}, "")
+		Expect(key).To(Equal("abc"))
 	})
 })
