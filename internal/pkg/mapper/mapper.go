7c74366844a2d4c67b4623beb6dc9b2cdd2cd84b
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-03-12 14:24:59 -0700

    mapper interface
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

---
+++ internal/pkg/mapper/mapper.go
@@ -0,0 +1,30 @@
+package mapper
+
+import (
+	"fmt"
+
+	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
+)
+
+
+// Mapper defines the interface that Maps an incoming request to an aggregation key
+type Mapper interface {
+	GetKey(node core.Node, typeURL string) (string, error)
+}
+
+type mapper struct {
+	config aggregationv1.KeyerConfiguration
+}
+
+// NewMapper construts a concrete implementation for the Mapper interface
+func NewMapper(config aggregationv1.KeyerConfiguration) Mapper {
+	return &mapper{
+		config: config,
+	}
+}
+
+// GetKey converts a request into an aggregated key
+func (mapper *mapper) GetKey(node core.Node, typeURL string) (string, error) {
+	return "", fmt.Errorf("Cannot map the input to a key")
+}
