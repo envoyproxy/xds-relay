b6ce444f172b884ba1a31cd60c4712d0555a2e12
Lisa Lu <lisalu@lyft.com>
2020-03-13 10:55:11 -0700

    PR comments
    Signed-off-by: Lisa Lu <lisalu@lyft.com>

--- internal/app/cache/cache.go
+++ internal/app/cache/cache.go
@@ -1,7 +1,7 @@
 package cache
 
 import (
-	envoy_service_discovery_v3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 )
 
 type Cache interface {
@@ -9,7 +9,7 @@ type Cache interface {
 	Exists(key string) bool
 
 	// Fetch returns the cached response if it exists.
-	Fetch(key string) (*envoy_service_discovery_v3.DiscoveryResponse, error)
+	Fetch(key string) (*envoy_api_v2.DiscoveryResponse, error)
 
 	// IsStreamOpen returns false if no stream is opened against the upstream control plane for the given key.
 	IsStreamOpen(key string) (bool, error)
@@ -18,14 +18,14 @@ type Cache interface {
 	SetStreamOpen(key string, open bool) error
 
 	// SetResponse sets the cache response and returns the list of open watches.
-	SetResponse(key string, resp envoy_service_discovery_v3.DiscoveryResponse) ([]*envoy_service_discovery_v3.DiscoveryRequest, error)
+	SetResponse(key string, resp envoy_api_v2.DiscoveryResponse) ([]*envoy_api_v2.DiscoveryRequest, error)
 
 	// ClearWatches discards the existing watches.
 	ClearWatches(key string) error
 
 	// AddWatch adds the watch to the cache and returns whether a stream is open.
-	AddWatch(key string, req envoy_service_discovery_v3.DiscoveryRequest) (bool, error)
+	AddWatch(key string, req envoy_api_v2.DiscoveryRequest) (bool, error)
 
 	// IsWatchPresent returns whether the given request is watching for the resource corresponding to the given key.
-	IsWatchPresent(key string, req envoy_service_discovery_v3.DiscoveryRequest) (bool, error)
+	IsWatchPresent(key string, req envoy_api_v2.DiscoveryRequest) (bool, error)
 }
