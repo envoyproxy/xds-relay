d8cbc84715d8cd243b939628da3d90326ae6af97
Lisa Lu <lisalu@lyft.com>
2020-03-13 10:56:36 -0700

    PR comments
    Signed-off-by: Lisa Lu <lisalu@lyft.com>

--- internal/app/cache/cache.go
+++ internal/app/cache/cache.go
@@ -11,12 +11,6 @@ type Cache interface {
 	// Fetch returns the cached response if it exists.
 	Fetch(key string) (*envoy_api_v2.DiscoveryResponse, error)
 
-	// IsStreamOpen returns false if no stream is opened against the upstream control plane for the given key.
-	IsStreamOpen(key string) (bool, error)
-
-	// SetStreamOpen sets whether there is a stream open against the upstream control plane for the given key.
-	SetStreamOpen(key string, open bool) error
-
 	// SetResponse sets the cache response and returns the list of open watches.
 	SetResponse(key string, resp envoy_api_v2.DiscoveryResponse) ([]*envoy_api_v2.DiscoveryRequest, error)
 
