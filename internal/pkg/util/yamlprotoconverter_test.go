208e473f296015ba0e1bf39223a9bb2e5d41bb25
eapolinario <eapolinario@lyft.com>
2020-03-15 14:12:27 -0700

    Move initial tests to the positive/negative BDD-style tests
    Signed-off-by: eapolinario <eapolinario@lyft.com>

--- internal/pkg/util/yamlprotoconverter_test.go
+++ internal/pkg/util/yamlprotoconverter_test.go
@@ -1,7 +1,6 @@
 package yamlprotoconverter_test
 
 import (
-	"fmt"
 	. "github.com/envoyproxy/xds-relay/internal/pkg/util"
 	aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
 	. "github.com/onsi/ginkgo"
@@ -18,71 +17,71 @@ type FragmentRule = aggregationv1.KeyerConfiguration_Fragment_Rule
 type MatchPredicate = aggregationv1.MatchPredicate
 type RequestTypeMatch = aggregationv1.MatchPredicate_RequestTypeMatch
 
-var s1 = `
-string_fragment: abc
-`
-
-var _ = Describe("Yamlprotoconverter", func() {
-	type TestCase struct {
-		YAML             string
-		ProtoToUnmarshal proto.Message
-		ExpectedProto    proto.Message
-	}
-	DescribeTable("should be able to convert from yaml to proto",
-		func(tc TestCase) {
-			fmt.Println(tc.YAML)
-			err := FromYAMLToProto(tc.YAML, tc.ProtoToUnmarshal)
-			Expect(err).To(BeNil())
-			Expect(proto.Equal(tc.ProtoToUnmarshal, tc.ExpectedProto)).To(Equal(true))
-		},
-		Entry("test 1", TestCase{
-			YAML: `
+var positiveTests = []TableEntry{
+	{
+		Description: "test result predicate containing a string fragment",
+		Parameters: []interface{}{
+			`
 string_fragment: abc
 `,
-			ProtoToUnmarshal: &ResultPredicate{},
-			ExpectedProto: &ResultPredicate{
+			&ResultPredicate{},
+			&ResultPredicate{
 				Type: &StringFragment{
 					StringFragment: "abc",
 				},
 			},
-		}),
-		// 		Entry("test 2", `
-		// fragments:
-		// - rules:
-		//   - match:
-		//       request_type_match:
-		//         types:
-		//         - type.googleapis.com/envoy.api.v2.Endpoint
-		//         - type.googleapis.com/envoy.api.v2.Listener
-		//     result:
-		//       string_fragment: "abc"
-		// `,
-		// 			KeyerConfiguration{},
-		// 			KeyerConfiguration{
-		// 				Fragments: []*Fragment{
-		// 					{
-		// 						Rules: []*FragmentRule{
-		// 							{
-		// 								Match: &MatchPredicate{
-		// 									Type: &aggregationv1.MatchPredicate_RequestTypeMatch_{
-		// 										RequestTypeMatch: &RequestTypeMatch{
-		// 											Types: []string{
-		// 												"type.googleapis.com/envoy.api.v2.Endpoint",
-		// 												"type.googleapis.com/envoy.api.v2.Listener",
-		// 											},
-		// 										},
-		// 									},
-		// 								},
-		// 								Result: &ResultPredicate{
-		// 									Type: &StringFragment{
-		// 										StringFragment: "abc",
-		// 									},
-		// 								},
-		// 							},
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 		),
-	)
+		},
+	},
+	{
+		Description: "test 2",
+		Parameters: []interface{}{
+			`
+fragments:
+- rules:
+  - match:
+      request_type_match:
+        types:
+        - type.googleapis.com/envoy.api.v2.Endpoint
+        - type.googleapis.com/envoy.api.v2.Listener
+    result:
+      string_fragment: "abc"
+`,
+			&KeyerConfiguration{},
+			&KeyerConfiguration{
+				Fragments: []*Fragment{
+					{
+						Rules: []*FragmentRule{
+							{
+								Match: &MatchPredicate{
+									Type: &aggregationv1.MatchPredicate_RequestTypeMatch_{
+										RequestTypeMatch: &RequestTypeMatch{
+											Types: []string{
+												"type.googleapis.com/envoy.api.v2.Endpoint",
+												"type.googleapis.com/envoy.api.v2.Listener",
+											},
+										},
+									},
+								},
+								Result: &ResultPredicate{
+									Type: &StringFragment{
+										StringFragment: "abc",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+var _ = Describe("Yamlprotoconverter", func() {
+	DescribeTable("should be able to convert from yaml to proto",
+		func(yml string, protoToUnmarshal proto.Message, expectedProto proto.Message) {
+			err := FromYAMLToProto(yml, protoToUnmarshal)
+			Expect(err).To(BeNil())
+			Expect(proto.Equal(protoToUnmarshal, expectedProto)).To(Equal(true))
+		},
+		positiveTests...)
 })
