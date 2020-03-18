391cd50f42fa2f1eaa8b02b0ec10214461571a2d
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-03-18 16:01:49 -0700

    add struct
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- internal/app/stream/multiplexer.go
+++ internal/app/stream/multiplexer.go
@@ -4,9 +4,31 @@ import (
 	"context"
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"google.golang.org/grpc"
 )
 
-// Multiplexer layer
+// Multiplexer handles the connections to the upstream control plane
 type Multiplexer interface {
-	QueueDiscoveryRequest(context.Context, chan *v2.DiscoveryRequest, chan *v2.DiscoveryResponse)
+	// QueueRequest starts a stream with the upstream control plane
+	QueueRequest(context.Context, chan *v2.DiscoveryRequest, chan *v2.DiscoveryResponse)
+}
+
+type multiplexer struct {
+	conn *grpc.ClientConn
+}
+
+// NewMux creates an instance based on the typeUrl of the resource
+// A new instance of Multiplexer is recommended per xds type
+// Eg. For eds requests of different services, create an instance each.
+func NewMux(ctx context.Context, conn *grpc.ClientConn, typeURL string) (Multiplexer, error) {
+	return &multiplexer{
+		conn: conn,
+	}, nil
+}
+
+func (m *multiplexer) QueueRequest(
+	ctx context.Context,
+	requestChan chan *v2.DiscoveryRequest,
+	responseChan chan *v2.DiscoveryResponse) {
+
 }
