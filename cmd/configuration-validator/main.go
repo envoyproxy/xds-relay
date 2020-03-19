5712eee00cfc7aa67279fa60ac0a01f23f0f62b7
eapolinario <eapolinario@lyft.com>
2020-03-18 22:37:44 -0700

    Configuration validator initial commit
    Signed-off-by: eapolinario <eapolinario@lyft.com>

---
+++ cmd/configuration-validator/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	// aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
+)
+
+func main() {
+	// Load yaml string from flags
+
+	// if err != nil, write error to stdout (stderr?) and error out
+	fmt.Println("wow")
+}
