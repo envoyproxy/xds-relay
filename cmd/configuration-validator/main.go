f0a6c58a52f886d15d57ce459958741ba62fed6c
eapolinario <eapolinario@lyft.com>
2020-03-19 01:29:21 -0700

    Add validator skeleton.
    Signed-off-by: eapolinario <eapolinario@lyft.com>

--- cmd/configuration-validator/main.go
+++ cmd/configuration-validator/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
-	"fmt"
-	// aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/envoyproxy/xds-relay/internal/pkg/util"
+	aggregationv1 "github.com/envoyproxy/xds-relay/pkg/api/aggregation/v1"
 )
 
-func main() {
-	// Load yaml string from flags
+type KeyerConfiguration = aggregationv1.KeyerConfiguration
 
-	// if err != nil, write error to stdout (stderr?) and error out
-	fmt.Println("wow")
+func main() {
+	var yamlFilename string
+	flag.StringVar(&yamlFilename, "yaml", "", "configuration yaml filename")
+	flag.Parse()
+
+	if yamlFilename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Load yaml string from flags
+	yamlFileContent, err := ioutil.ReadFile(yamlFilename)
+	if err != nil {
+		// TODO add a more descriptive error message
+		log.Fatal(err)
+	}
+
+	// Load config file into a valid KeyerConfiguration object and,
+	// in case we encounter an error, print the error string to
+	// stderr.
+	var config KeyerConfiguration
+	err = yamlproto.FromYAMLToKeyerConfiguration(string(yamlFileContent), &config)
+	if err != nil {
+		// TODO add a more descriptive error message. Mention that the validation stops
+		// after the first error.
+		log.Fatal(err)
+	}
 }
