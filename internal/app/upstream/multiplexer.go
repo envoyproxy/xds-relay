ea400db84c2c57b26494fcb4276c9d99792aa0b2
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-03-23 14:03:22 -0700

    typo
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- internal/app/upstream/multiplexer.go
+++ internal/app/upstream/multiplexer.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Multiplexer handles the requests and responses from the origin server.
-// The multiplexer handles each xds request on a separate streams,
+// The multiplexer handles each xds request on a separate stream,
 // e.g. 2 different cds requests happen on 2 separate streams.
 // It is the caller's responsibility to make sure there is one instance of Multipler per unique xds request.
 type Multiplexer interface {
@@ -19,7 +19,7 @@ type Multiplexer interface {
 	// QueueRequest uses the retry and timeout configurations to make best effort to get the responses from origin server.
 	// If there's a new request in between retries, the retries are abandoned.
 	// The request and response happen asynchronously. Retries are scoped for sending messages to origin server.
-	// If the timeouts are exhausted, receive fails or a irrecoverable error occurs, the error is sent back.
+	// If the timeouts are exhausted, receive fails or a irrecoverable error occurs, the error is sent back to the caller.
 	// It is the caller's responsibility to send a new request from the last known DiscoveryRequest.
 	// Cancellation and cleanup operations will be based on cancellation of the context and closing of channels.
 	QueueRequest(context.Context, chan *v2.DiscoveryRequest, chan *Response) error
