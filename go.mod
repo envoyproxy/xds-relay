13be2a1af68bdd03c5ae1472a01de8509944a571
Lisa Lu <lisalu@lyft.com>
2020-03-24 11:37:54 -0700

    LRU cache implementation (#30)
    This PR implements the basic methods defined in the cache interface using the [groupcache/lru library](https://godoc.org/github.com/golang/groupcache/lru). In addition to eviction that occurs when a cache receives new items once full that traditionally happens with an LRU cache, users may configure a TTL after which an individual entry will expire.
    
    #9 
    
    Signed-off-by: Lisa Lu <lisalu@lyft.com>

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/envoyproxy/go-control-plane v0.9.4
 	github.com/envoyproxy/protoc-gen-validate v0.3.0
 	github.com/ghodss/yaml v1.0.0
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e
 	github.com/golang/protobuf v1.4.0-rc.4
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
+	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.14.0
 	google.golang.org/grpc v1.27.1
 	google.golang.org/protobuf v1.20.1
--- internal/app/cache/cache.go
+++ internal/app/cache/cache.go
@@ -1,19 +1,143 @@
+// Package cache provides a public interface and implementation for an in-memory cache that keeps the most recent
+// response from the control plane per aggregated key.
 package cache
 
 import (
-	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang/groupcache/lru"
+
+	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 )
 
 type Cache interface {
-	// Exists returns true if the key exists in the cache, false otherwise.
-	Exists(key string) bool
-
 	// Fetch returns the cached response if it exists.
-	Fetch(key string) (*envoy_api_v2.DiscoveryResponse, error)
+	Fetch(key string) (*v2.DiscoveryResponse, error)
 
-	// SetResponse sets the cache response and returns the list of open watches.
-	SetResponse(key string, resp envoy_api_v2.DiscoveryResponse) ([]*envoy_api_v2.DiscoveryRequest, error)
+	// SetResponse sets the cache response and returns the list of requests.
+	SetResponse(key string, resp v2.DiscoveryResponse) ([]*v2.DiscoveryRequest, error)
 
-	// AddWatch adds the watch to the cache and returns whether a stream is open.
-	AddWatch(key string, req envoy_api_v2.DiscoveryRequest) (bool, error)
+	// AddRequest adds the request to the cache.
+	AddRequest(key string, req v2.DiscoveryRequest) error
+}
+
+type cache struct {
+	cacheMu sync.RWMutex
+	cache   lru.Cache
+	ttl     time.Duration
+}
+
+type Resource struct {
+	resp           *v2.DiscoveryResponse
+	requests       []*v2.DiscoveryRequest
+	expirationTime time.Time
+}
+
+// Callback function for each eviction. Receives the key and cache value when called.
+type onEvictFunc func(key string, value Resource)
+
+func NewCache(maxEntries int, onEvicted onEvictFunc, ttl time.Duration) (Cache, error) {
+	if ttl < 0 {
+		return nil, fmt.Errorf("ttl must be nonnegative but was set to %v", ttl)
+	}
+	return &cache{
+		cache: lru.Cache{
+			// Max number of cache entries before an item is evicted. Zero means no limit.
+			MaxEntries: maxEntries,
+			// OnEvict is called for each eviction.
+			OnEvicted: func(cacheKey lru.Key, cacheValue interface{}) {
+				key, ok := cacheKey.(string)
+				if !ok {
+					panic(fmt.Sprintf("Unable to cast key %v to string upon eviction", cacheKey))
+				}
+				value, ok := cacheValue.(Resource)
+				if !ok {
+					panic(fmt.Sprintf("Unable to cast value %v to resource upon eviction", cacheValue))
+				}
+				onEvicted(key, value)
+			},
+		},
+		// Duration before which an item is evicted for expiring. Zero means no expiration time.
+		ttl: ttl,
+	}, nil
+}
+
+func (c *cache) Fetch(key string) (*v2.DiscoveryResponse, error) {
+	c.cacheMu.Lock()
+	defer c.cacheMu.Unlock()
+	value, found := c.cache.Get(key)
+	if !found {
+		return nil, fmt.Errorf("no value found for key: %s", key)
+	}
+	resource, ok := value.(Resource)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast cache value to type resource for key: %s", key)
+	}
+	// Lazy eviction based on TTL occurs here. Fetch does not increase the lifespan of the key.
+	if resource.isExpired(time.Now()) {
+		c.cache.Remove(key)
+		return nil, nil
+	}
+	return resource.resp, nil
+}
+
+func (c *cache) SetResponse(key string, resp v2.DiscoveryResponse) ([]*v2.DiscoveryRequest, error) {
+	c.cacheMu.Lock()
+	defer c.cacheMu.Unlock()
+	value, found := c.cache.Get(key)
+	if !found {
+		resource := Resource{
+			resp:           &resp,
+			expirationTime: c.getExpirationTime(time.Now()),
+		}
+		c.cache.Add(key, resource)
+		return nil, nil
+	}
+	resource, ok := value.(Resource)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast cache value to type resource for key: %s", key)
+	}
+	resource.resp = &resp
+	resource.expirationTime = c.getExpirationTime(time.Now())
+	c.cache.Add(key, resource)
+	// TODO: Add logic that allows for notifying of watches.
+	return resource.requests, nil
+}
+
+func (c *cache) AddRequest(key string, req v2.DiscoveryRequest) error {
+	c.cacheMu.Lock()
+	defer c.cacheMu.Unlock()
+	value, found := c.cache.Get(key)
+	if !found {
+		resource := Resource{
+			requests:       []*v2.DiscoveryRequest{&req},
+			expirationTime: c.getExpirationTime(time.Now()),
+		}
+		c.cache.Add(key, resource)
+		return nil
+	}
+	resource, ok := value.(Resource)
+	if !ok {
+		return fmt.Errorf("unable to cast cache value to type resource for key: %s", key)
+	}
+	resource.requests = append(resource.requests, &req)
+	resource.expirationTime = c.getExpirationTime(time.Now())
+	c.cache.Add(key, resource)
+	return nil
+}
+
+func (r *Resource) isExpired(currentTime time.Time) bool {
+	if r.expirationTime.IsZero() {
+		return false
+	}
+	return r.expirationTime.Before(currentTime)
+}
+
+func (c *cache) getExpirationTime(currentTime time.Time) time.Time {
+	if c.ttl > 0 {
+		return currentTime.Add(c.ttl)
+	}
+	return time.Time{}
 }
