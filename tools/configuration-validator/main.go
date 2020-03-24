49548a6a648d17494cb1a0b826dd5d455b9dd0c6
eapolinario <eapolinario@lyft.com>
2020-03-24 15:57:01 -0700

    Copy README into long description of cobra command.
    Signed-off-by: eapolinario <eapolinario@lyft.com>

--- tools/configuration-validator/main.go
+++ tools/configuration-validator/main.go
@@ -20,14 +20,16 @@ var (
 		Use:   "configuration-validator",
 		Short: "A tool to help validate aggregation key configuration files",
 		Long: `configuration-validator is a CLI tool used to validate aggregation key yaml files.
-The aggregation key yaml file is validated against the proto file defined in
-https://github.com/envoyproxy/xds-relay/blob/master/api/protos/aggregation/v1/aggregation.proto.
-The proto file is annotated with https://github.com/envoyproxy/protoc-gen-validate/#constraint-rules
-constraint rules to enforce semantic rules, e.g.,
-The CLI tool uses the same validation method as the one found in xds-relay initialization. Having a
-separate tool to validate config files enables external processes to consume and generate only valid
-files. For example, imagine a scenario where an automated process generates yaml and a CI pipeline
-uses the validator tool to ensure the generated yaml is valid.`,
+The aggregation key yaml file is validated against the
+[https://github.com/envoyproxy/xds-relay/blob/master/api/protos/aggregation/v1/aggregation.proto](proto file).
+The same proto file is annotated with
+[protoc-gen-validate constraint rules](https://github.com/envoyproxy/protoc-gen-validate/#constraint-rules)
+to enforce semantic rules. Both the xds-relay initialization and the CLI share the same validation method.
+
+The idea behind having a separate CLI tool to validate yaml files is to enable tooling around the generation
+and validation of config files. For example, imagine a scenario where an automated process generates a
+config file and a CI pipeline uses the validator tool to ensure the generated yaml is valid.
+`,
 		Run: func(cmd *cobra.Command, args []string) {
 			yamlFileContent, err := ioutil.ReadFile(cfgFile)
 			if err != nil {
