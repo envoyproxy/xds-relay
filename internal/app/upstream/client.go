2bdd04e2cf094701a2904ff0af1d108cad354269
eapolinario <eapolinario@lyft.com>
2020-07-09 12:07:20 -0700

    Remove unused import
    Signed-off-by: eapolinario <eapolinario@lyft.com>

--- internal/app/upstream/client.go
+++ internal/app/upstream/client.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	backoff "github.com/cenkalti/backoff"
 	"github.com/envoyproxy/xds-relay/internal/app/metrics"
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
