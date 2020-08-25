3f61cdbba9514594b3a5bfa7e71088be3dc3f85f
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-08-25 11:08:38 -0700

    lint
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- internal/app/transport/watch.go
+++ internal/app/transport/watch.go
@@ -3,52 +3,52 @@ package transport
 import (
 	"fmt"
 
-	gcp "github.com/envoyproxy/go-control-plane/pkg/cache/v2"
+	gcpv2 "github.com/envoyproxy/go-control-plane/pkg/cache/v2"
 	gcpv3 "github.com/envoyproxy/go-control-plane/pkg/cache/v2"
 )
 
 // Watch interface abstracts v2 and v3 watches
 type Watch interface {
 	Close()
-	GetChannelV2() chan gcp.Response
+	GetChannelV2() chan gcpv2.Response
 	GetChannelV3() chan gcpv3.Response
 	Send(s interface{}) (bool, error)
 }
 
-var _ Watch = &WatchV2{}
+var _ Watch = &watchV2{}
 
 // WatchV2 is the transport object that takes care of send responses to the xds clients
-type WatchV2 struct {
-	Req *gcp.Request
-	out chan gcp.Response
+type watchV2 struct {
+	Req *gcpv2.Request
+	out chan gcpv2.Response
 }
 
 // NewWatchV2 creates a new watch object
-func NewWatchV2(req *gcp.Request) Watch {
-	return &WatchV2{
+func NewWatchV2(req *gcpv2.Request) Watch {
+	return &watchV2{
 		Req: req,
-		out: make(chan gcp.Response, 1),
+		out: make(chan gcpv2.Response, 1),
 	}
 }
 
 // Close closes the communication with the xds client
-func (w *WatchV2) Close() {
+func (w *watchV2) Close() {
 	close(w.out)
 }
 
 // GetChannelV2 gets the v2 channel used for communication with the xds client
-func (w *WatchV2) GetChannelV2() chan gcp.Response {
+func (w *watchV2) GetChannelV2() chan gcpv2.Response {
 	return w.out
 }
 
 // GetChannelV3 gets the v3 channel used for communication with the xds client
-func (w *WatchV2) GetChannelV3() chan gcpv3.Response {
+func (w *watchV2) GetChannelV3() chan gcpv3.Response {
 	return nil
 }
 
 // Send sends the xds response over wire
-func (w *WatchV2) Send(s interface{}) (bool, error) {
-	resp, ok := s.(gcp.Response)
+func (w *watchV2) Send(s interface{}) (bool, error) {
+	resp, ok := s.(gcpv2.Response)
 	if !ok {
 		return false, fmt.Errorf("payload %s could not be casted to DiscoveryResponse", s)
 	}
