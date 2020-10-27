bd2b2d956a259b61b0cfee2bcf16ea9d6af40894
Jyoti Mahapatra <jmahapatra@lyft.com>
2020-10-27 11:06:48 -0700

    nolint
    Signed-off-by: Jyoti Mahapatra <jmahapatra@lyft.com>

--- internal/app/transport/watchv2.go
+++ internal/app/transport/watchv2.go
@@ -31,7 +31,6 @@ func (w *watchV2) GetChannel() *ChannelVersion {
 // Send sends the xds response over wire
 func (w *watchV2) Send(s Response) bool {
 	select {
-	//nolint
 	case w.out <- &gcpv2.PassthroughResponse{DiscoveryResponse: s.Get().V2, Request: s.GetRequest().V2}:
 		return true
 	default:
