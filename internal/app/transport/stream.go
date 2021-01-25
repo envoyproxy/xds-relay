a5c0d4655c26b8775cb43f1a6dd5767e66e98510
Jess Yuen <jyuen@lyft.com>
2021-01-25 08:58:02 -0800

    WIP
    Signed-off-by: Jess Yuen <jyuen@lyft.com>

--- internal/app/transport/stream.go
+++ internal/app/transport/stream.go
@@ -1,8 +1,16 @@
 package transport
 
+import (
+	"context"
+	"time"
+
+	status "google.golang.org/genproto/googleapis/rpc/status"
+)
+
 // Stream abstracts the grpc client stream and DiscoveryRequest/Response
 type Stream interface {
-	SendMsg(version string, nonce string) error
+	SendMsg(version string, nonce string, errorDetail *status.Status) error
 	RecvMsg() (Response, error)
+	RecvMsgWithTimeout(ctx context.Context, timeout time.Duration) (Response, error)
 	CloseSend() error
 }
